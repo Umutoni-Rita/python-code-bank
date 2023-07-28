391bfb6e44d0bae8488fde8f81666aff5e7a3089
Umutoni-Rita <umurita37@gmail.com>
2023-07-28 18:13:57 +0200

    Number Guessing Game

---
+++ number_guessing.py
@@ -0,0 +1,23 @@
+import random
+
+def number_guessing():
+    secret_number = random.randint(1, 20)
+    attempts = 0
+
+    print("---Welcome to the Number Guessing Game---")
+    print("I've picked a number between 1 and 20. GUess it!")
+
+    while True:
+        guess = int(input("Your guess: "))
+        attempts += 1
+
+        if guess == secret_number:
+            print(f"Congratulations! YOu guessed the number in {attempts} attempts")
+            break
+        elif guess < secret_number:
+            print("Too low. Try again.")
+        else:
+            print("Too high. Try again.")
+
+if __name__ == "__main__":
+    number_guessing()
